35a5e84089ea2344cd7a21265b16c492ec6b8379
said aziz <aziwazaziz2003@gmail.com>
2024-10-29 01:42:28 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      red: "hsl(0, 94%, 66%)",
+      Blue: " hsl(231, 69%, 60%)",
+      gray: "hsl(229, 8%, 60%)",
+      Grayish_Blue: " hsl(229, 8%, 90%)",
+      Very_Dark_Blue: "hsl(229, 31%, 21%)",
+    },
+    fontFamily: {
+      font_family1: ["Rubik", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
